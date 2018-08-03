683c04d770fee5a52e3a4a814ea6bc511458f397
Henrique Dias <hacdias@gmail.com>
2018-08-03 13:34:25 +0100

    compatible with caching

--- index.js
+++ index.js
@@ -7,69 +7,78 @@ const defaultState = {
   error: null
 }
 
-module.exports = {
-  name: 'ipfs',
+module.exports = function () {
+  let ready = false
 
-  persistActions: [
-    'IPFS_INIT_FINISHED',
-    'IPFS_API_UPDATED'
-  ],
+  return {
+    name: 'ipfs',
 
-  reducer (state, {type, payload, error}) {
-    state = state || defaultState
+    persistActions: [
+      'IPFS_INIT_FINISHED',
+      'IPFS_API_UPDATED'
+    ],
 
-    if (type === 'IPFS_INIT_STARTED') {
-      return Object.assign({}, state, { error: null })
+    reducer (state, {type, payload, error}) {
+      state = state || defaultState
+
+      if (type === 'IPFS_INIT_STARTED') {
+        return Object.assign({}, state, { error: null })
+      }
+
+      if (type === 'IPFS_INIT_FINISHED') {
+        ready = true
+        return Object.assign({}, state, { identity: payload })
+      }
+
+      if (type === 'IPFS_INIT_FAILED') {
+        ready = false
+        return Object.assign({}, state, { error: error })
+      }
+
+      if (type === 'IPFS_API_UPDATED') {
+        return Object.assign({}, state, { apiAddress: payload, error: null })
+      }
+
+      return state
+    },
+
+    getExtraArgs () {
+      return { getIpfs: () => root.ipfs }
+    },
+
+    selectIpfsReady: () => ready,
+
+    selectIpfsApiAddress: state => state.ipfs.apiAddress,
+
+    selectIpfsInitFailed: state => !!state.ipfs.error,
+
+    selectIpfsIdentity: state => state.ipfs.identity,
+
+    doInitIpfs: () => async ({ dispatch, getState }) => {
+      dispatch({ type: 'IPFS_INIT_STARTED' })
+
+      const { apiAddress } = getState().ipfs
+      let identity = null
+
+      try {
+        root.ipfs = await getIpfs({ api: true, ipfs: apiAddress })
+        // will fail if remote api is not available on default port
+        identity = await root.ipfs.id()
+      } catch (error) {
+        return dispatch({ type: 'IPFS_INIT_FAILED', error })
+      }
+
+      dispatch({ type: 'IPFS_INIT_FINISHED', payload: identity })
+    },
+
+    doUpdateIpfsAPIAddress: (apiAddress) => ({dispatch, store}) => {
+      // root.ipfs needs to be set to null so getIpfs doesn't
+      // use the previous connection.
+      root.ipfs = null
+      ready = false
+
+      dispatch({type: 'IPFS_API_UPDATED', payload: apiAddress})
+      store.doInitIpfs()
     }
-
-    if (type === 'IPFS_INIT_FINISHED') {
-      return Object.assign({}, state, { ipfsReady: true, identity: payload })
-    }
-
-    if (type === 'IPFS_INIT_FAILED') {
-      return Object.assign({}, state, { ipdaReady: false, error: error })
-    }
-
-    if (type === 'IPFS_API_UPDATED') {
-      return Object.assign({}, state, { apiAddress: payload, error: null })
-    }
-
-    return state
-  },
-
-  getExtraArgs () {
-    return { getIpfs: () => root.ipfs }
-  },
-
-  selectIpfsReady: state => state.ipfs.ipfsReady,
-
-  selectIpfsApiAddress: state => state.ipfs.apiAddress,
-
-  selectIpfsInitFailed: state => !!state.ipfs.error,
-
-  selectIpfsIdentity: state => state.ipfs.identity,
-
-  doInitIpfs: () => async ({ dispatch, getState }) => {
-    dispatch({ type: 'IPFS_INIT_STARTED' })
-    console.log('IPFS_INIT_STARTED')
-    const {apiAddress} = getState().ipfs
-    let identity = null
-    try {
-      root.ipfs = await getIpfs({ api: true, ipfs: apiAddress })
-      // will fail if remote api is not available on default port
-      identity = await root.ipfs.id()
-    } catch (error) {
-      return dispatch({ type: 'IPFS_INIT_FAILED', error })
-    }
-    dispatch({ type: 'IPFS_INIT_FINISHED', payload: identity })
-    console.log('IPFS_INIT_FINISHED')
-  },
-
-  doUpdateIpfsAPIAddress: (apiAddress) => ({dispatch, store}) => {
-    dispatch({type: 'IPFS_API_UPDATED', payload: apiAddress})
-    // root.ipfs needs to be set to null so getIpfs doesn't
-    // use the previous connection.
-    root.ipfs = null
-    store.doInitIpfs()
   }
 }
